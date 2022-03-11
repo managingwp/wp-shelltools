da6638c41f89d839795a6bb86c25e3580078a85a
Jordan Trask <jtrask@gmail.com>
2022-03-11 16:11:32 -0500

    Updated

--- gp-logcode.sh
+++ gp-logcode.sh
@@ -3,19 +3,45 @@
 . $(dirname "$0")/functions.sh
 _debug "Loading functions.sh"
 
+# -- getopts
+while getopts "c:m:l:d:r:ez" option; do
+        case ${option} in
+                c) LOGCODE=$OPTARG ;;
+                m) MODE=$OPTARG ;;
+                l) LOGFILE=$OPTARG ;;
+                d) DOMAIN=$OPTARG ;;
+		r) RESULTS=$OPTARG ;;
+                e) GP_EXCLUDE=1 ;;
+                z) Z_INCLUDE=1 ;;
+                ?) usage ;;
+        esac
+done
+
+_debug "args: $@"
+_debug "getopts: \$LOGCODE=$LOGCODE \$MODE=$MODE \$LOGFILE=$LOGFILE \$DOMAIN=$DOMAIN \$RESULTS=$RESULTS \$GP_EXCLUDE=$GP_EXCLUDE \$Z_INCLUDE=$Z_INCLUDE"
+_debug_all
+
+
 # Usage
 usage () {
-        echo "Usage: $SCRIPT_NAME logcode -c <code> (-l <logfilename>|-a) [-e] [-r results]"
-        echo "  -c <code> = the http status code number 4 = 4xx, 5 = 5xx, 404"
-        echo "  -l <logfilename> = specific log file or -a for all"
-        echo "  -a = all nginx log files"
+        echo "Usage: $SCRIPT_NAME -c <code> -m [all|domain|log] (-l <logfilename>|-d <domain>) [-r results] [-e]"
 	echo ""
-        echo "  Optional"
+	echo "Commands:"
+        echo "  -c <code> = the http status code to locate number 4 = 4xx, 5 = 5xx, 404"
+	echo "  -m <mode>		- Log Method"
+	echo "					- all = all logs"
+	echo "					- domain = domain logs"
+	echo "					- log = single log, requires -l"
+        echo "  -l <logfilename> 	- Specific log file when using -m log"
+        echo "	-d <domain>		- Domain logs when using -m domain"
+        echo ""
+        echo "  Options"
         echo "    -r [results] = number of results, optional, default 40"
         echo "    -e = exclude GridPane, staging and canary."
+        echo "    -z = include compressed files"
 }
 
-
+# -- check_logs
 check_logs () { # Nginx or OLS?
 	_debug "check_logs()"
 	_debug "Checking if logs exist"
@@ -26,156 +52,138 @@ check_logs () { # Nginx or OLS?
 
 	if [ -d $nginxlogs ]; then
 	        _debug "Found nginx logs under $nginxlogs"
-	        logfiledir=$nginxlogs
+	        LOGFILEDIR=$nginxlogs
 	elif [ -d $olslogs ]; then
 	        _debug "Found ols logs under $olslogs"
-	        logfiledir=$olslogs
+	        LOGFILEDIR=$olslogs
 	elif [[ $TEST ]]; then
 		_debug "In test mode, using test data"
-		logfiledir=$SCRIPTPATH/tests
+		LOGFILEDIR=$SCRIPTPATH/tests
 	else
 	        _error "No $nginxlogs or $olslogs directory"
 	        exit
 	fi
 }
 
-main () {
-	_debug "main()"
+process_options () {
+	# Confirm exclude
+	if [[ $GP_EXCLUDE == "-e" ]]; then
+		_debug "argument -e set - exclude gp, staging and canary sites in $LOGFILEDIR/*"
+               	EXCLUDE="egrep -v '/access.log$|staging|canary|gridpane|.gz'"
+        else
+              	EXCLUDE=""
+        fi
+                
 	# Set results if not defined.
-	if [ -z $results ];then
-		results="40"
-	fi
-	_debug "results=$results"
+        if [ -z $RESULTS ];then
+                results="40"
+        fi
+        _debug "RESULTS=$RESULTS"
+}
 
-	# All logs or just one?
-	if [[ $alllogs == "1" ]]; then
-		_debug "argument -a set - all log file mode"
-	        # Exclude gridpane specific logs
-	        if [[ $exclude == "-e" ]]; then
-	                _debug "argument -e set - exclude gp, staging and canary sites in $logfiledir/*"
-	                files=$(ls -aSd $logfiledir/* | grep access | egrep -v '/access.log$|staging|canary|gridpane|.gz')
-	        else
-	        	_debug "go through all logs in $logfiledir/*"
-	                files=$(ls -aSd $logfiledir/* | egrep -v '.gz')
-	        fi
-	        _debug "logfiles selected"
-	        _debug "$files"
+# -- main
+main () {
+	_debug_function
 	
-	# Just one log file.
-	else
-	        _debug "single log file mode"
-	        _debug "checking log files "
-	        _debug " - $logfilename"
-	        if [ -f $logfiledir/$logfile ]; then
-	                _success "Log file exists - $logfile"
-	                files=$(ls -aSd $logfiledir/$logfile)
+	# Process options
+	process_options
+
+	if [[ $1 == "all" ]]; then
+		# Mode all
+		_debug "All logs mode"
+		if [[ $Z_INCLUDE ]]; then
+			_debug "Include compressed files"
+                	PROCESS_FILES=$(ls -aSd $LOGFILEDIR/* | $EXCLUDE)
+                	_debug "PROCESS_FILES: $PROCESS_FILES"
+                else
+                	_debug "Exclude compressed files"
+	                PROCESS_FILES=$(ls -aSd $LOGFILEDIR/*.log | $EXCLUDE)
+	                _debug "PROCESS_FILES: $PROCESS_FILES"	                
+	        fi
+	elif [[ $1 == "domain" ]]; then
+		# Mode domain
+		_debug "Domain logs mode"
+		if [[ $Z_INCLUDE ]]; then
+			_debug "Include compressed files"
+			PROCESS_FILES=$(ls -aSd $LOGFILEDIR/$DOMAIN* | $EXCLUDE)
+			_debug "PROCESS_FILES: $PROCESS_FILES"4
+		else
+			_debug "Exclude compressed files"
+			PROCESS_FILES=$(ls -aSd $LOGFILEDIR/*.log | $EXCLUDE)
+			_debug "PROCESS_FILES: $PROCESS_FILES"
+		fi		
+	elif [[ $1 == "log" ]]; then
+		# Mode log
+	        _debug "Single logfile mode - $LOGFILE"
+	        if [[ -f $LOGFILEDIR/$LOGFILE ]]; then
+	                _success "Log file exists - $logfile"	                
+	                PROCESS_FILES=$LOGFILEDIR/$LOGFILE
 	        else
-	                _error "Log file $logfiledir/$logfile doesn't exist"
+	                _error "Log file $LOGFILEDIR/$logfile doesn't exist"
 	                exit
 	        fi
 	fi
 	
 	# logcode
-	if [[ $logcode -lt 10 ]]; then
+	if [[ $LOGCODE -lt 10 ]]; then
 		_debug "logcode is less than 10"
 		logcode="$logcode[0-9][0-9]"
 	fi
 
 	for file in $files; do
 	        _debug "Processing $file"
-	        _debug "Running -- grep \" $logcode \" $file | awk '{ print \$6\" - \"\$10\" - \"\$7\" \"$8\" \"\$9}' | sort | uniq -c | sort -nr | head -$results"
-	        content=$(grep "\" $logcode " $file | awk '{ print $6" - "$10" - "$7" "$8" "$9}' | sort | uniq -c | sort -nr | head -$results)
+		if [[ $Z_INCLUDE == "1" ]]; then
+                        _debug "Running -- zgrep \" $logcode \" $file | awk '{ print $6" - "$10" - "$7" "$8" "$9}' | sort | uniq -c | sort -nr | head -$RESULTS)"
+                        content=$(zgrep "\" $logcode " $file | awk '{ print $6" - "$10" - "$7" "$8" "$9}' | sort | uniq -c | sort -nr | head -$RESULTS)
+		else
+			_debug "Running -- grep \" $logcode \" $file | awk '{ print $6" - "$10" - "$7" "$8" "$9}' | sort | uniq -c | sort -nr | head -$RESULTS)"
+		        content=$(grep "\" $logcode " $file | awk '{ print $6" - "$10" - "$7" "$8" "$9}' | sort | uniq -c | sort -nr | head -$RESULTS)
+		fi	
+		# Print content
 	        if [[ $content ]]; then
 	        	echo "$content"
-		        echo "...more lines but limited to top $results"
+		        echo "...more lines but limited to top $RESULTS"
 		else
 			_debug "No data in $file"
 		fi
 	done
 }
 
-# --
-# Parse Arguments
-# --
+# -------
+# -- Main
+# -------
 
-# -allow a command to fail with !’s side effect on errexit
-# -use return value from ${PIPESTATUS[0]}, because ! hosed $?
-! getopt --test > /dev/null 
-if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-    echo 'I’m sorry, `getopt --test` failed in this environment.'
-    exit 1
-fi
-
-OPTIONS=c:l:aer:d
-LONGOPTS=logcode:,logfile:,all,exclude,results,debug:
-
-# -regarding ! and PIPESTATUS see above
-# -temporarily store output to be able to check for errors
-# -activate quoting/enhanced mode (e.g. by writing out “--options”)
-# -pass arguments only via   -- "$@"   to separate them correctly
-! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
-if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-    # e.g. return value is 1
-    #  then getopt has complained about wrong arguments to stdout
-    exit 2
-fi
-# read getopt’s output this way to handle the quoting right:
-eval set -- "$PARSED"
-
-c=- l=- a=n e=n r=-
-# now enjoy the options in order and nicely split until we see --
-# logcode -c <code> (-l <logfilename>|-a) [-e] [-r results]
-while true; do
-    case "$1" in
-        -c|--logcode)
-            logcode=$2
-            shift 2
-            ;;
-        -l|--logfile)
-            logfile=$2
-            shift 2
-            ;;
-        -a|--all)
-            alllogs="1"
-            shift 1
-            ;;
-        -e|--exclude)
-            exclude="1"
-            shift 1
-            ;;
-        -r|--results)
-            results=$2
-            shift 2
-            ;;
-        -d|--debug)
-            DEBUG="1"
-            shift 1
-            ;;
-        --)
-            shift
-            break
-            ;;
-        *)
-            echo "Programming error"
-            exit 3
-            ;;
-    esac
-done
-
-# handle non-option arguments
-_debug "logcode: $logcode, logfile: $logfile, alllogs: $alllogs, exclude:$exclude, results:$results"
-_debug "args: $@"
-if [[ -z $logcode ]]; then
-	usage
-	_error "No http code provided"
-	exit 4
-elif [[ -z $logfile ]] && [[ -z $alllogs ]]; then
-	usage
-	_error "Specify a -l <logfile> or -a for all logs"
-	exit 4
-fi
-
-
-_debug_all $@
 check_logs
-main
+
+if [[ -z $LOGCODE ]] || [[ -z $MODE ]]; then
+        usage
+        _error "Need to specify -c <code> and -m <method>"
+        exit
+fi
+
+if [[ $MODE == "all" ]]; then
+	# Log all
+	_debug "mode: $MODE"
+	main all
+elif [[ $MODE == "domain" ]]; then
+	# Log domain
+        _debug "mode: $MODE"
+        if [[ $DOMAIN ]]; then
+                _debug "domain: $DOMAIN"
+                main domain
+        else
+                _error "Need to specify domain via -d <domain>"
+        fi
+elif [[ $MODE == "log" ]]; then
+	# Log mode
+        _debug "mode: $MODE"
+        if [[ $LOGFILE ]]; then
+                _debug "logfile: $LOGFILE"
+                main log $LOGFILE
+        else
+                _error "Need to specify logfile via -l <logfile>"
+        fi
+else
+	usage
+fi
+
