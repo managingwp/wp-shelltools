809c996892143205e7e46a00e340142a25409bbc
Jordan Trask <jtrask@gmail.com>
2023-04-21 15:59:36 -0400

    Added domain name of WordPress install to syslog messages

--- scripts/cron-shim.sh
+++ scripts/cron-shim.sh
@@ -19,15 +19,20 @@ POST_CRON_CMD="" # - Command to run after cron completes
 # Check if $WP_ROOT contains a WordPress install
 [[ WP_ROOT_INSTALL=$(wp --skip-plugins --skip-themes core is-installed --path=$WP_ROOT 2> /dev/null) ]] || { echo "Error: $WP_ROOT is not a WordPress install.\n$WP_ROOT_INSTALL " >&2; exit 1; }
 
+# Get the domain name of the WordPress install
+DOMAIN_NAME=$($WP_CLI --skip-plugins --skip-themes option get siteurl 2> /dev/null | grep -oP '(?<=//)[^/]+')
+[[ $? -ne 0 ]]; && { echo "Error: Could not get domain name.\n$DOMAIN_NAME"; exit 1 }
+
 # Log the start time
 START_TIME=$(date +%s.%N)
 
 # Run WordPress crons due now and log the output
-CRON_OUTPUT=$($WP_CLI --skip-plugins --skip-theme cron event run --due-now --path=$WP_ROOT 2>&1)
+CRON_OUTPUT=$($WP_CLI --skip-plugins --skip-themes cron event run --due-now --path=$WP_ROOT 2>&1)
 
 # Check if there was an error running wp-cli command
 if [[ $? -ne 0 ]]; then
     echo "Error: wp-cli command failed" >&2
+    echo "$CRON_OUTPUT"
     if [[ -n "$POST_CRON_CMD" ]]; then
         eval "$POST_CRON_CMD"
     fi
@@ -46,7 +51,7 @@ CPU_USAGE=$(ps -p $$ -o %cpu | tail -n 1)
 
 # Check if logging to syslog is enabled
 if [[ $LOG_TO_SYSLOG == "1" ]]; then
-    logger -t "wordpress-crons" "Cron job completed in $TIME_SPENT seconds with $CPU_USAGE% CPU usage. \nOutput: $CRON_OUTPUT"
+    echo -e "Cron job completed in $TIME_SPENT seconds with $CPU_USAGE% CPU usage. \nOutput: $CRON_OUTPUT" | logger -t "wordpress-crons-$DOMAIN_NAME"
 elif [[ $LOG_TO_FILE == "1" ]]; then
     # Log to file in the WordPress install directory
     echo "$(date +"%Y-%m-%d %H:%M:%S") - WordPress crons completed in $TIME_SPENT seconds with $CPU_USAGE% CPU usage. \nOutput: $CRON_OUTPUT" >> $LOG_FILE
