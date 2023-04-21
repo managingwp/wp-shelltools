7b5949e4e3e8e4459737a57fed3a99d765b52ed9
Jordan Trask <jtrask@gmail.com>
2023-04-21 15:25:25 -0400

    * Updated cron-shim.sh to enable/disable log file and syslog, check if WordPress is installed and if WordPress directory exists. * Added example GridPane nginx log format output.

--- scripts/cron-shim.sh
+++ scripts/cron-shim.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 # -- Created by Jordan - hello@managingwp.io - https://managingwp.io
+# TODO check if WordPress is installed before running cron
+# TODO check if WP_ROOT directory exists before running cron
 
 # Set up the necessary variables
-WP_CLI=/usr/local/bin/wp # - Location of wp-cli
-WP_ROOT=/path/to/wordpress # - Path to WordPress
-LOG_TO_SYSLOG=0 # - Log to syslog? 0 = no, 1 = yes
-LOG_FILE=${WP_ROOT}/wordpress-crons.log # Location for wordpress cron.
+WP_CLI="/usr/local/bin/wp" # - Location of wp-cli
+WP_ROOT="/path/to/wordpress" # - Path to WordPress
+LOG_TO_SYSLOG="0" # - Log to syslog? 0 = no, 1 = yes
+LOG_TO_FILE="0" # - Log to file? 0 = no, 1 = yes
+LOG_FILE="${WP_ROOT}/wordpress-crons.log" # Location for wordpress cron.
 HEARTBEAT_URL="https://betteruptime.com/api/v1/heartbeat/v25v234v4634b636v3" # - Heartbeat monitoring URL
 POST_CRON_CMD="" # - Command to run after cron completes
 
 # Check if wp-cli is installed
-if ! [ -x "$(command -v $WP_CLI)" ]; then
-  echo 'Error: wp-cli is not installed.' >&2
-  exit 1
-fi
+#[[ $(command -v $WP_CLI) ]]  || { echo 'Error: wp-cli is not installed.' >&2; exit 1; }
+
+# Check if $WP_ROOT exists
+#[[ -d "$WP_ROOT" ]] || { echo "Error: $WP_ROOT does not exist." >&2; exit 1; }
+
+# Check if $WP_ROOT contains a WordPress install
+[[ $(wp core is-installed --path=$WP_ROOT 2> /dev/null) ]] || { echo "Error: $WP_ROOT is not a WordPress install." >&2; exit 1; }
 
 # Log the start time
 START_TIME=$(date +%s.%N)
@@ -23,16 +29,16 @@ CRON_OUTPUT=$($WP_CLI cron event run --due-now --path=$WP_ROOT 2>&1)
 
 # Check if there was an error running wp-cli command
 if [[ $? -ne 0 ]]; then
-  echo "Error: wp-cli command failed" >&2
-  if [[ -n "$POST_CRON_CMD" ]]; then
-    eval "$POST_CRON_CMD"
-  fi
-  exit 1
+    echo "Error: wp-cli command failed" >&2
+    if [[ -n "$POST_CRON_CMD" ]]; then
+        eval "$POST_CRON_CMD"
+    fi
+    exit 1
 fi
 
 # Check if heartbeat monitoring is enabled and send a request to the heartbeat URL if it is and there are no errors
 if [[ -n "$HEARTBEAT_URL" ]] && [[ $? -eq 0 ]] ; then
-  curl -I -s "$HEARTBEAT_URL" > /dev/null
+    curl -I -s "$HEARTBEAT_URL" > /dev/null
 fi
 
 # Log the end time and CPU usage
@@ -41,9 +47,9 @@ TIME_SPENT=$(echo "$END_TIME - $START_TIME" | bc)
 CPU_USAGE=$(ps -p $$ -o %cpu | tail -n 1)
 
 # Check if logging to syslog is enabled
-if [[ $LOG_TO_SYSLOG == 1 ]]; then
-  logger -t "wordpress-crons" "Cron job completed in $TIME_SPENT seconds with $CPU_USAGE% CPU usage. Output: $CRON_OUTPUT"
-else
-  # Log to file in the WordPress install directory
-  echo "$(date +"%Y-%m-%d %H:%M:%S") - WordPress crons completed in $TIME_SPENT seconds with $CPU_USAGE% CPU usage. Output: $CRON_OUTPUT" >> $LOG_FILE
+if [[ $LOG_TO_SYSLOG == "1" ]]; then
+    logger -t "wordpress-crons" "Cron job completed in $TIME_SPENT seconds with $CPU_USAGE% CPU usage. \nOutput: $CRON_OUTPUT"
+elif [[ $LOG_TO_FILE == "1" ]]; then
+    # Log to file in the WordPress install directory
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - WordPress crons completed in $TIME_SPENT seconds with $CPU_USAGE% CPU usage. \nOutput: $CRON_OUTPUT" >> $LOG_FILE
 fi
--- wpst-goaccess.sh
+++ wpst-goaccess.sh
@@ -18,24 +18,29 @@ DEBUG_ON="0"
 # ------------
 
 usage () {
-    echo "Usage: wpst-goaccess [-d|-c] [-f (nginx|ols) [-domain domain.com|--all]"
-	echo ""
-	echo "This script will try and detect log files in common locations, you can also"
-	echo "specify the platform and format using the options below"
-	echo ""
-	echo "  Commands"
-	echo "    -domain <domain>              - Domain name of log files to process"
-    echo "    -all                          - Go through all the logs versus a single domain"
-	echo ""
-    echo "  Options:"
-	echo "    -ld         - Specify directory of log files"
-	echo "    -l          - Specify log file"
-	echo "    -p          - Specify platform (gridpane|runcloud)"
-    echo "    -f          - Override detected format, (nginx|ols)"
-    echo "    -c          - Process compressed log files"
-    echo "    -d          - Debug"
-    echo "    -dr         - Dry Run"
-    echo ""
+	USAGE=\
+"Usage: wpst-goaccess [-h|-d|-c|-dr|-f (nginx|ols)|-p [(gridpane|runcloud)] [-ld <log directory>|-l <log file>|-time <timerange>] [-domain domain.com|--all|-file <filename> [-p (gridpane|runcloud)]
+	
+    This script will try and detect log files in common locations, you can also
+    specify the platform and format using the options below
+	
+    Commands
+        -domain <domain>              - Domain name of log files to process
+        -all                          - Go through all the logs versus a single log file
+        -file <filename>              - Process a single file
+        -time <timerange>             - Specify a time range to process dates in the format yyyy-mm-dd-hh-mm-ss,yyyy-mm-dd-hh-mm-ss (e.g. 2017-01-01-00-00-00,2017-01-01-23-59-59)
+                                      - First portion is start time and second part is end time.
+	
+    Options:
+        -h          - Help		
+        -d          - Debug
+        -dr         - Dry Run
+        -p          - Specify platform (gridpane|runcloud)
+        -f          - Override detected format, (nginx|ols)
+        -t          - Use test log files
+
+    "
+	echo "$USAGE"
 }
 
 # -- check_goaccess
@@ -74,14 +79,15 @@ set_format () {
 
 	# GRIDPANE-NGINX
 	# ./common/logging.conf:log_format we_log '[$time_local] $remote_addr $upstream_response_time $upstream_cache_status $http_host "$request" $status $body_bytes_sent $request_time "$http_referer" "$http_user_agent"';
-	if [[ $FORMAT == "GRIDPANE-NGINX" ]]; then
+	# [14/Apr/2023:06:30:32 -0500] 127.0.0.1 1.732 - domain.com "GET /?pwgc=1628918241 HTTP/2.0" 200 39563 1.731 "-" "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm) Chrome/103.0.5060.134 Safari/537.36"
+	if [[ $FORMAT == "GPNGINX" ]]; then
 		LOG_FORMAT='[%d:%t %^] %h %^ - %v \"%r\" %s %b \"%R\" \"%u\"\'
 		DATE_FORMAT='%d/%b/%Y'
 		TIME_FORMAT='%H:%M:%S %Z'
 	fi
 
 	# GRIDPANE-OLS
-	if [[ $FORMAT == "GRIDPANE-OLS" ]]; then
+	if [[ $FORMAT == "GPOLS" ]]; then
         LOG_FORMAT='[%d:%t %^] %h %^ - %v \"%r\" %s %b \"%R\" \"%u\"\'
         DATE_FORMAT='%d/%b/%Y'
         TIME_FORMAT='%H:%M:%S %Z'
@@ -92,28 +98,33 @@ set_format () {
 
 detect_logs () {
 	echo "Detecting log files"
+	# TODO figure out how to test and set the format better.
 
 	# GRIDPANE-OLS
     if [[ -d /usr/local/lsws ]] && [[ -d /var/www/ ]]; then
         echo "Detected GridPane OLS logs"
-        FORMAT="OLS"
+        FORMAT="GPOLS"
         if [[ $ACTION == "ALL" ]]; then
             LOG_FILE_LOCATION="/var/www/*/logs"
             LOG_FILTER="*.access.log*gz"
         elif [[ $ACTION == "DOMAIN" ]]; then
             LOG_FILE_LOCATION="/var/www/$DOMAIN/logs"
             LOG_FILTER="*.access.log"
-        fi
+        elif [[ $ACTION == "FILE" ]]; then
+			LOG_FILE_LOCATION="$FILE"
+		fi
     elif [[ -d /var/log/nginx ]] && [[ -d /var/www/ ]]; then
         echo "Detected GridPane NGINX logs"
-        FORMAT="NGINX"
+        FORMAT="GPNGINX"
         if [[ $ACTION == "ALL" ]]; then
             LOG_FILE_LOCATION="/var/log/nginx"
             LOG_FILTER="*.access.log*gz"
         elif [[ $ACTION == "DOMAIN" ]]; then
             LOG_FILE_LOCATION="/var/log/nginx"
             LOG_FILTER="*${DOMAIN}*.access.log"
-        fi
+        elif [[ $ACTION == "FILE" ]]; then
+			LOG_FILE_LOCATION="$FILE"
+		fi
 	# OLS
     elif [[ -d /usr/local/lsws ]]; then
         echo "Detected OLS logs"
@@ -124,42 +135,113 @@ detect_logs () {
     	elif [[ $ACTION == "DOMAIN" ]]; then
 	    	LOG_FILE_LOCATION="/var/www/$DOMAIN/logs"
 	    	LOG_FILTER="*.access.log"
-	    fi
+	    elif [[ $ACTION == "FILE" ]]; then
+			LOG_FILE_LOCATION="$FILE"		
+		fi
 	# NGINX
     elif [[ -d /var/log/nginx ]]; then
         echo "Detected GridPane Nginx logs"
+		FORMAT="OLS"
     	LOG_FILE_LOCATION="/var/log/nginx"
 
 		if [[ $ACTION == "ALL" ]]; then
 		   	LOG_FILTER="*.access.log"
 		elif [[ $ACTION == "DOMAIN" ]]; then
 			LOG_FILTER="$DOMAIN.access.log"
+		elif [[ $ACTION == "FILE" ]]; then
+			LOG_FILE_LOCATION="$FILE"
 		fi
     else
     	_error "Can't detect webserver logs"
     	exit
     fi
+
+	_debug "FORMAT:$FORMAT LOG_FILE_LOCATION:$LOG_FILE_LOCATION LOG_FILTER:$LOG_FILTER"
 }
 
-# -- do_goaccess
-do_goaccess () {
+# -- collect_logs
+collect_logs () {
+	local CATCMD="cat"
 	_debug "Format: $FORMAT Log File Location:$LOG_FILE_LOCATION Log Filter: $LOG_FILTER"
-
+	LOG_COLLECT_DATA=$(mktemp)
 	if [[ $ACTION == "DOMAIN" ]]; then
 		if [[ $DRY_RUN == "1" ]]; then
 			ls -al ${LOG_FILE_LOCATION}/${LOG_FILTER}
-			echo "cat ${LOG_FILE_LOCATION}/${LOG_FILTER} | goaccess --log-format='$LOG_FORMAT' --date-format='$DATE_FORMAT' --time-format='$TIME_FORMAT'"
 		else
-			cat ${LOG_FILE_LOCATION}/${LOG_FILTER} | goaccess --log-format="${LOG_FORMAT}" --date-format="$DATE_FORMAT" --time-format="$TIME_FORMAT"
+			cat ${LOG_FILE_LOCATION}/${LOG_FILTER} > $LOG_COLLECT_DATA
 		fi
 	elif [[ $ACTION == "ALL" ]]; then
 		if [[ $DRY_RUN == "1" ]]; then
 			ls -al ${LOG_FILE_LOCATION}/${LOG_FILTER}
-			echo "zcat ${LOG_FILE_LOCATION}/${LOG_FILTER} | goaccess --log-format='$LOG_FORMAT' --date-format='$DATE_FORMAT' --time-format='$TIME_FORMAT'"
 		else
-			( cat ${LOG_FILE_LOCATION}/*.access.log; zcat ${LOG_FILE_LOCATION}/${LOG_FILTER} ) | goaccess --log-format="$LOG_FORMAT" --date-format="$DATE_FORMAT" --time-format="$TIME_FORMAT"
+			cat ${LOG_FILE_LOCATION}/*.access.log > $LOG_COLLECT_DATA; zcat ${LOG_FILE_LOCATION}/${LOG_FILTER} >> $LOG_COLLECT_DATA
+		fi
+	elif [[ $ACTION == "FILE" ]]; then
+		if [[ $DRY_RUN == "1" ]]; then
+			ls -al ${LOG_FILE_LOCATION}
+		else
+			[[ $LOG_FILE_LOCATION == "*.gz" ]] && CATCMD="zcat"
+			$CATCMD ${LOG_FILE_LOCATION} > $LOG_COLLECT_DATA
 		fi
 	fi
+	LOG_DATA_FILE=$LOG_COLLECT_DATA
+}
+
+# -- do_goaccess
+do_goaccess () {
+	_debug "Proceeding with d_goaccess"
+
+	if [[ $ACTION == "DOMAIN" ]]; then
+		if [[ $DRY_RUN == "1" ]]; then
+			echo "cat ${LOG_FILE_LOCATION}/${LOG_FILTER} | goaccess --log-format='$LOG_FORMAT' --date-format='$DATE_FORMAT' --time-format='$TIME_FORMAT'"
+		else
+			cat $LOG_DATA_FILE | goaccess --log-format="${LOG_FORMAT}" --date-format="$DATE_FORMAT" --time-format="$TIME_FORMAT"
+		fi
+	elif [[ $ACTION == "ALL" ]]; then
+		if [[ $DRY_RUN == "1" ]]; then
+			echo "cat $LOG_DATA_FILE | goaccess --log-format='$LOG_FORMAT' --date-format='$DATE_FORMAT' --time-format='$TIME_FORMAT'"
+		else
+			cat $LOG_DATA_FILE | goaccess --log-format="$LOG_FORMAT" --date-format="$DATE_FORMAT" --time-format="$TIME_FORMAT"
+		fi
+	elif [[ $ACTION == "FILE" ]]; then
+		[[ $LOG_FILE_LOCATION == "*.gz" ]] && CATCMD="zcat"
+		if [[ $DRY_RUN == "1" ]]; then
+			echo "cat $LOG_DATA_FILE | goaccess --log-format='$LOG_FORMAT' --date-format='$DATE_FORMAT' --time-format='$TIME_FORMAT'"
+		else
+			cat $LOG_DATA_FILE | goaccess --log-format="$LOG_FORMAT" --date-format="$DATE_FORMAT" --time-format="$TIME_FORMAT"
+		fi
+	elif [[ $ACTION == "TEST" ]]; then
+		[[ $LOG_FILE_LOCATION == "*.gz" ]] && CATCMD="zcat"
+		if [[ $DRY_RUN == "1" ]]; then
+			echo "cat ${LOG_FILE_LOCATION} | goaccess --log-format='$LOG_FORMAT' --date-format='$DATE_FORMAT' --time-format='$TIME_FORMAT'"
+		else
+			$CATCMD ${LOG_FILE_LOCATION} | goaccess --log-format="$LOG_FORMAT" --date-format="$DATE_FORMAT" --time-format="$TIME_FORMAT"
+		fi
+	else
+		echo "No action specified"
+		exit
+	fi
+}
+
+function sed_logs() {
+	_debug "Processing logs using custom time - $CUSTOM_TIME"
+	SED_LOG=$(mktemp)
+	if ! [[ $CUSTOM_TIME =~ ^[0-9]{2}/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{2}/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2}$ ]]; then
+    	echo "Error: Please provide dates in the format dd/Mon/yyyy:hh:mm:ss"
+    	exit 1
+	fi
+
+	local EDATE=$(echo $CUSTOM_TIME | cut -d, -f1)
+	local SDATE=$(echo $CUSTOM_TIME | cut -d, -f2)
+    local START_DATE=$(date -d "$(echo "$SDATE" | sed 's/\// /g;s/:/ /')" +%s | xargs -I{} date -d "@{}" +'%d\/%b\/%Y:%H:%M:%S')
+	local END_DATE=$(date -d "$(echo "$EDATE" | sed 's/\// /g;s/:/ /')" +%s | xargs -I{} date -d "@{}" +'%d\/%b\/%Y:%H:%M:%S')
+
+	if [[ $DRY_RUN == "1" ]]; then
+		echo "sed -n "/$START_DATE/,/$END_DATE/ p" $LOG_DATA_FILE > $SED_LOG"
+	else
+		sed -n "/$START_DATE/,/$END_DATE/ p" $LOG_DATA_FILE > $SED_LOG
+	fi	
+	LOG_DATA=$SED_LOG
 }
 
 # ---------------
@@ -171,24 +253,26 @@ while [[ $# -gt 0 ]]
 do
 key="$1"
 case $key in
+    -h)
+    ACTION="HELP"
+	DCMD+="ACTION=$2 "
+    shift # past argument
+    ;;
     -platform)
     PLATFORM="$2"
+	DCMD+="PLATFORM=$2 "
     shift # past argument
     shift # past value
     ;;
 	-f|--format)
 	FORMAT="$2"
+	DCMD+="FORMAT=$2 "
 	shift # past argument
 	shift # past value
 	;;
     -d|--debug)
     DEBUG_ON="1"
     DCMD+="DEBUG_ON=1 "
-    shift # past argument
-    ;;
-    -c|--compess)
-	DCMD+="COMPRESS_FILES=1 "
-    COMPRESS_FILES="1"
     shift # past argument
     ;;
     -dr)
@@ -199,7 +283,7 @@ case $key in
     -domain)
     ACTION="DOMAIN"
     DOMAIN="$2"
-    DCMD+="ACTION=DOMAIN DOMAIN=$2 "
+    DCMD+="ACTION=DOMAIN DOMAIN=$2"
     shift # past argument
     shift # past value
     ;;
@@ -208,6 +292,26 @@ case $key in
     DCMD+="ACTION=ALL "
     shift # past argument
     ;;
+	-file)
+    ACTION="FILE"
+	FILE="$2"
+    DCMD+="ACTION=FILE FILE=$2 "
+    shift # past argument
+	shift # past value
+    ;;
+	-t)
+    ACTION="TEST"
+	FILE="$2"
+    DCMD+="ACTION=TEST FILE=$2"
+    shift # past argument
+	shift # past value
+    ;;
+	-time)
+	CUSTOM_TIME="$2"
+	DCMD+="CUSTOM_TIME=$2"
+	shift # past argument
+	shift # past value
+	;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
     shift # past argument
@@ -217,9 +321,9 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 _debug "Running wpst-goaccess $DCMD"
-_debug_all $@
+_debug_all "${*}"
 
-if [[ -z $ACTION ]]; then
+if [[ -z $ACTION ]] || [[ $ACTION == "HELP" ]]; then
 	usage
 	echo "Error: No action specified"
     exit
@@ -233,5 +337,7 @@ else
 	check_goaccess
 	detect_logs
 	set_format
-	do_goaccess $ACTION
+	collect_logs
+	[[ ! -z $CUSTOM_TIME ]] && sed_logs $CUSTOM_TIME
+	do_goaccess $ACTION $LOG_DATA_FILE
 fi
